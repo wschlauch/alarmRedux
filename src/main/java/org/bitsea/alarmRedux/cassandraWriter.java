75fe0b821361b3b38a6cc466422e72b717ae3b06
wschlauch <wschlauch@gmail.com>
2016-12-01 17:21:48 +0100

    removed unnecessary import

--- src/main/java/org/bitsea/alarmRedux/cassandraWriter.java
+++ src/main/java/org/bitsea/alarmRedux/cassandraWriter.java
@@ -2,7 +2,6 @@ package org.bitsea.alarmRedux;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
